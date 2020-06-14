56635ff9b12d119ef5149c3047b105647650d971
ren-renren <ren.alpineski.1003@gmail.com>
2020-06-14 15:28:28 +0900

    Initial Commit

---
+++ Myjanken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Myjanken
+//
+//  Created by 鈴木蓮 on 2020/06/14.
+//  Copyright © 2020 鈴木蓮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
