7f982308f2cf48a37750f5471f2d7b32c14d442e
ren-renren <ren.alpineski.1003@gmail.com>
2021-01-10 15:52:38 +0900

    お試しでコミット。

--- Myjanken/ViewController.swift
+++ Myjanken/ViewController.swift
@@ -16,5 +16,32 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var answerImageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    //じゃんけん（数字）
+    var answerNumber = 0
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        if answerNumber == 0 {
+            //グー
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named:"gu")
+        
+        } else if answerNumber == 1 {
+            
+            //チョキ
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named:"choki")
+            
+        } else if answerNumber == 2 {
+            
+            //パー
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named:"pa")
+            
+        }
+        //次のじゃんけんへ
+        answerNumber = answerNumber + 1
+    }
 }
-
